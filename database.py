25860d876fdffc361f88b4c11f3df7cc6536a00f
sickk <66170734+sickkxx@users.noreply.github.com>
2026-04-01 18:24:07 -0300

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="ta123",
+        password="SUA_SENHA",
         database="ebay_produtos"
     )
 
